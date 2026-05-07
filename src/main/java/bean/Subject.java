321fede6033171de7d21143e5550f32b67b405ac
n_imai <n_imai@LAPTOP-0FD6KVHA>
2026-05-07 11:37:20 +0900

    追加メッセージ

---
+++ src/main/java/bean/Subject.java
@@ -0,0 +1,50 @@
+package bean;
+
+import java.io.Serializable;
+
+public class Subject implements Serializable {
+
+	/**
+	 * 学校コード:String
+	 */
+	private String cd;
+
+	/**
+	 * 学校名:String
+	 */
+	private String name;
+
+	/**
+	 * 学校：school
+	 */
+	private School school;
+
+	/**
+	 * ゲッタ・セッタ
+	 */
+	public String getCd() {
+		return cd;
+	}
+
+	public void setCd(String cd) {
+		this.cd = cd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public School getSchool() {
+		return school;
+	}
+	public void setSchool(School school) {
+		this.school = school;
+	}
+
+
+
+}
---
+++ src/main/java/bean/TestListSubject.java
@@ -0,0 +1,68 @@
+package bean;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class TestListSubject implements Serializable {
+	
+	/**
+	 * 入学年度：int
+	 */
+	private int entYear;
+
+	/**
+	 * 学生番号：String
+	 */
+	private String studentNo;
+	
+	/**
+	 * 学生名：String
+	 */
+	private String studentName;
+
+	/**
+	 * クラス番号：String
+	 */
+	private String classNum;
+
+	/**
+	 * 
+	 */
+	private Map<Integer, Integer> points;
+	
+	/**
+	 * ゲッタ・セッタ
+	 */
+	public int getEntYear() {
+		return entYear;
+	}
+
+	public void setEntYear(int entYear) {
+		this.entYear = entYear;
+	}
+	
+	public String getstudentNo() {
+		return studentNo;
+	}
+
+	public void setStudentNo(String studentno) {
+		this.studentNo = studentno;
+		
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getClassNum() {
+		return classNum;
+	}
+
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
+	}
+
+
+}
