6926d0decf422e4214c863ef5b8af0169579422f
k_kikuchi <k_kikuchi@LAPTOP-EUUA9U91>
2026-05-07 12:36:55 +0900

    TestListSubjectの書き換え

--- src/main/java/bean/TestListSubject.java
+++ src/main/java/bean/TestListSubject.java
@@ -4,77 +4,80 @@ import java.io.Serializable;
 import java.util.Map;
 
 public class TestListSubject implements Serializable {
-	
-	/**
-	 * 入学年度：int
-	 */
-	private int entYear;
 
-	/**
-	 * 学生番号：String
-	 */
-	private String studentNo;
-	
-	/**
-	 * 学生名：String
-	 */
-	private String studentName;
+    /**
+     * 入学年度：int
+     */
+    private int entYear;
 
-	/**
-	 * クラス番号：String
-	 */
-	private String classNum;
+    /**
+     * 学生番号：String
+     */
+    private String studentNo;
 
-	/**
-	 * 
-	 */
-	private Map<Integer, Integer> points;
-	
-	/**
-	 * ゲッタ・セッタ
-	 */
-	public int getEntYear() {
-		return entYear;
-	}
+    /**
+     * 学生名：String
+     */
+    private String studentName;
 
-	public void setEntYear(int entYear) {
-		this.entYear = entYear;
-	}
-	
-	public String getstudentNo() {
-		return studentNo;
-	}
+    /**
+     * クラス番号：String
+     */
+    private String classNum;
 
-	public void setStudentNo(String studentno) {
-		this.studentNo = studentno;
-		
-	}
-	public String getStudentName() {
-		return studentName;
-	}
+    /**
+     * 回数と点数のマップ
+     */
+    private Map<Integer, Integer> points;
 
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
-	public String getClassNum() {
-		return classNum;
-	}
+    /**
+     * 【重要】DAOのエラーを解消するためのメソッド
+     * 内部のMapに回数と点数を格納します
+     */
+    public void putPoint(int key, int value) {
+        this.points.put(key, value);
+    }
 
-	public void setClassNum(String classNum) {
-		this.classNum = classNum;
-	}
+    /**
+     * ゲッタ・セッタ
+     */
+    public int getEntYear() {
+        return entYear;
+    }
 
-	public Map<Integer, Integer> getPoints() {
-		return points;
-	}
+    public void setEntYear(int entYear) {
+        this.entYear = entYear;
+    }
 
-	public void setPoints(Map<Integer, Integer> points) {
-		this.points = points;
-	}
+    public String getStudentNo() {
+        return studentNo;
+    }
 
-	public String getStudentNo() {
-		return studentNo;
-	}
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
 
+    public String getStudentName() {
+        return studentName;
+    }
 
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(String classNum) {
+        this.classNum = classNum;
+    }
+
+    public Map<Integer, Integer> getPoints() {
+        return points;
+    }
+
+    public void setPoints(Map<Integer, Integer> points) {
+        this.points = points;
+    }
 }
