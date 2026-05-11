b27ddf25cbb923768a9d703384de992167554a8c
k_kikuchi <k_kikuchi@LAPTOP-EUUA9U91>
2026-05-11 11:37:37 +0900

    q

--- src/main/java/bean/Subject.java
+++ src/main/java/bean/Subject.java
@@ -1,12 +1,14 @@
 package bean;
 
-<<<<<<< HEAD
-public class Subject {
+import java.io.Serializable;
+
+public class Subject implements Serializable {
 
     private String cd;
     private String name;
     private School school;
 
+    // ゲッタ・セッタ
     public String getCd() {
         return cd;
     }
@@ -31,53 +33,3 @@ public class Subject {
         this.school = school;
     }
 }
-=======
-import java.io.Serializable;
-
-public class Subject implements Serializable {
-
-	/**
-	 * 学校コード:String
-	 */
-	private String cd;
-
-	/**
-	 * 学校名:String
-	 */
-	private String name;
-
-	/**
-	 * 学校：school
-	 */
-	private School school;
-
-	/**
-	 * ゲッタ・セッタ
-	 */
-	public String getCd() {
-		return cd;
-	}
-
-	public void setCd(String cd) {
-		this.cd = cd;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public School getSchool() {
-		return school;
-	}
-	public void setSchool(School school) {
-		this.school = school;
-	}
-
-
-
-}
->>>>>>> branch 'master' of https://github.com/kikuchi0704/Exam.git
