f44e44647e5c59057ca9273616341019d0b3cede
n_imai <n_imai@LAPTOP-0FD6KVHA>
2026-05-14 11:51:18 +0900

    完成

--- src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -29,6 +29,8 @@ public class SubjectDeleteExecuteAction extends Action {
 			sDao.delete(subject);
 		}
 
+		System.out.println(teacher);
+
 		// JSPへフォワード（削除完了画面、または科目一覧へリダイレクト）
 		req.getRequestDispatcher("subject_delete_done.jsp").forward(req, res);
 	}
