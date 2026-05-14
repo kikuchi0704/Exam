8fff3deef06cd462243ea53fa0b9f8309966d933
n_imai <n_imai@LAPTOP-0FD6KVHA>
2026-05-14 11:07:04 +0900

    Merge branch 'master' of https://github.com/kikuchi0704/Exam.git

--- src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
-public class SubjectDelateExecuteAction extends Action {
+public class SubjectDeleteExecuteAction extends Action {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
